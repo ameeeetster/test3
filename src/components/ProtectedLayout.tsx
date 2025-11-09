f90b6e378617272335e5b15a560b2e22c9ee55ed
ameeeetster <ameeeetster@gmail.com>
2025-11-09 11:48:56 +0000

    Refresh auth state on session changes

--- src/components/ProtectedLayout.tsx
+++ src/components/ProtectedLayout.tsx
@@ -20,7 +20,7 @@ export function ProtectedLayout() {
     });
     return () => {
       mounted = false;
-      sub.subscription.unsubscribe();
+      sub?.subscription?.unsubscribe?.();
     };
   }, []);
 
